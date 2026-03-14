e80c34e4f4a11799ecd2b1f5e989f6d5c9ee381a
Dafa Ghaitsa Yogatama <yogatamadafa9@gmail.com>
2026-03-14 09:56:00 +0700

    fix:about me

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -2,9 +2,14 @@ import dafaImage from "@/assets/images/dafa.jpeg";
 
 export default function AboutSection() {
   return (
-    <section className="text-muted-foreground flex flex-col md:flex-row w-full max-w-6xl mx-auto px-6 justify-between items-center gap-8">
+    <section className="text-muted-foreground flex flex-col md:flex-row w-full mx-auto justify-between items-center gap-8">
       <div className="flex-col w-full">
-        <h1 className="w-full justify-start items-center text-4xl text-primary">About Me</h1>
+                <div className="w-full flex flex-wrap justify-between items-end gap-3">
+          <div className="flex flex-col gap-2">
+            <p className="text-sm uppercase tracking-[0.3em] text-primary/90">About Me</p>
+            <h2 className="text-4xl font-bold text-primary">My Journey</h2>
+          </div>
+        </div>
         <hr className="my-4 w-1/2" />
         <p className="w-full text-lg md:text-2xl">
           Hello my name is Dafa Ghaitsa, I am a website developer with a passion for creating beautiful and functional websites. I have experience in both frontend and backend development, and I am always looking for new challenges to improve my skills. This website is a showcase of my work and a place where I share my thoughts on web development and design.
